7b443befcc9755085c330c381219c5fbb176ff06
hrithikmordani <hrithik.mordani36@nmims.edu.in>
2022-01-05 21:45:56 +0530

    added basic google map

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, unused_import
 
 import 'package:final_food_delivery/combined_widget/custom_app_bar.dart';
 import 'package:final_food_delivery/constants/colors.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
 class LocationScreen extends StatelessWidget {
@@ -17,9 +18,59 @@ class LocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        title: Text('Location Screen'),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: GoogleMap(
+              myLocationEnabled: true,
+              initialCameraPosition:
+                  CameraPosition(target: LatLng(10, 10), zoom: 2),
+            ),
+          ),
+          Positioned(top: 40, left: 20, right: 20, child: LocationSearchBox()),
+          Positioned(
+              bottom: 40,
+              left: 20,
+              right: 20,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 70),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: kPrimaryColor),
+                  onPressed: () {},
+                  child: Text('Save'),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class LocationSearchBox extends StatelessWidget {
+  const LocationSearchBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Enter your location',
+          suffixIcon: Icon(Icons.search),
+          contentPadding: const EdgeInsets.only(left: 20, bottom: 5, right: 5),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.white)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.white)),
+        ),
       ),
     );
   }
